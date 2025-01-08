c7420d8c37e541276a2120a31645b5dc04423569
VanierVikings <vaniervikingrobotics@gmail.com>
2025-01-08 10:32:10 -0500

    setup Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,4 +1,11 @@
 package frc.robot;
 
+import com.pathplanner.lib.config.PIDConstants;
+
 public final class Constants {
+    public static class SwerveConstants {
+        public static final double MAX_SPEED = 3.0;
+        public static final PIDConstants autoDrivePID = new PIDConstants(5.0, 0.0, 0.0);
+        public static final PIDConstants autoRotationPID = new PIDConstants(5.0, 0.0, 0.0);
+    }
 }
