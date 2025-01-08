f6191b02727662cd2d5e9820544d8f1b73215999
Ben <benxinboliang@gmail.com>
2025-01-08 17:19:34 -0500

    max speed change

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,7 +16,7 @@ public final class Constants {
     public static final double LOOP_TIME = 0.13; // s, 20ms + 110ms sprk max velocity lag
 
     public static final class SwerveConstants {
-        public static final double MAX_SPEED  = Units.feetToMeters(14.5);
+        public static final double MAX_SPEED  = Units.feetToMeters(14);
         public static final PIDConstants autoDrivePID = new PIDConstants(5.0, 0.0, 0.0);
         public static final PIDConstants autoRotationPID = new PIDConstants(5.0, 0.0, 0.0);
         public static final double WHEEL_LOCK_TIME = 10; // seconds
